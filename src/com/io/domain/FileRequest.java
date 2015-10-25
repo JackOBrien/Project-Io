179c628e1fc683dc730cd39b4e711f52a0393431
Jack O'Brien <obrienjo@mail.gvsu.edu>
2015-10-25 13:14:49 -0400

    Renamed FILE_REQUEST to FILE_TRANSFER

--- src/com/io/domain/FileRequest.java
+++ src/com/io/domain/FileRequest.java
@@ -2,6 +2,6 @@ package com.io.domain;
 
 public class FileRequest extends Packet{
     public FileRequest(){
-        super(PacketType.FILE_REQUEST.id());
+        super(PacketType.FILE_TRANSFER.id());
     }
 }

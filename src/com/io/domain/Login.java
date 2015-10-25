7ee65d203ca07a1fc2cb91a3dae26ba120d299df
Jack O'Brien <obrienjo@mail.gvsu.edu>
2015-10-25 13:12:14 -0400

    Login now has userId

--- src/com/io/domain/Login.java
+++ src/com/io/domain/Login.java
@@ -2,6 +2,7 @@ package com.io.domain;
 
 public class Login extends Packet {
     private String username;
+    private int userId;
 
     public Login(int packetType, String username){
         super(packetType);
@@ -10,5 +11,13 @@ public class Login extends Packet {
 
     public String getUsername(){
         return this.username;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 }

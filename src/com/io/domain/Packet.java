d64e84a809e2a2d29f38ba830f7c14c697990c48
Jack O'Brien <obrienjo@mail.gvsu.edu>
2015-10-27 15:24:09 -0400

    Added userId to Packet

--- src/com/io/domain/Packet.java
+++ src/com/io/domain/Packet.java
@@ -4,10 +4,17 @@ import java.io.Serializable;
 
 public abstract class Packet implements Serializable {
 
+    private int userId;
+
     private int packetType;
 
-    public Packet(int packetType) {
+    public Packet(int userId, int packetType) {
+        this.userId = userId;
         this.packetType = packetType;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public int getPacketType() {

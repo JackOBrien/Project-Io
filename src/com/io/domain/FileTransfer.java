e9f04d205691a21fdaef55d70ac208b98b43f38a
Lawrence O'Boyle <oboylelii@gmail.com>
2015-11-01 16:11:10 -0500

    Allows for adding a file as data on the packet or as null

--- src/com/io/domain/FileTransfer.java
+++ src/com/io/domain/FileTransfer.java
@@ -1,7 +1,29 @@
 package com.io.domain;
 
+import java.io.File;
+import java.nio.file.Files;
+
 public class FileTransfer extends Packet{
+    byte[] content;
     public FileTransfer(int userId){
         super(userId, PacketType.FILE_TRANSFER);
+        this.content = null;
+    }
+    public FileTransfer(int userId, String filePath) throws java.io.IOException {
+        super(userId, PacketType.FILE_TRANSFER);
+
+        File file = new File(filePath);
+
+        try {
+            this.content = Files.readAllBytes(file.toPath());
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public void writeFile(String dest) throws java.io.IOException {
+        File file = new File(dest);
+        Files.write(file.toPath(), this.content);
     }
 }
